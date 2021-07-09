9a494dd28be64cdf8c1d24dbef21c32e5ba7e59b
Gulam Anas <g.anas786@gmail.com>
2021-07-09 19:28:17 +0530

    responsive

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const navSlide = () => {
+  const nav = document.querySelector(".left-nav");
+  const burger = document.querySelector(".burger");
+
+  nav.classList.toggle("nav-slider");
+  burger.classList.toggle("toggle");
+  
+};
+
 const showImg = (img) => {
   const image = document.querySelector(".bigimg");
   image.src = img.src;
